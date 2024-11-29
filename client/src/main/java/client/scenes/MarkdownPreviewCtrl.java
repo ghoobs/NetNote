304d663b55ccbf7e06e83d3ca49123538d72e06e
Tristan Fransen <t.a.w.d.fransen@student.tudelft.nl>
2024-11-29 15:51:44 +0100

    Updated markdown viewer to be a WebView control

--- client/src/main/java/client/scenes/MarkdownPreviewCtrl.java
+++ client/src/main/java/client/scenes/MarkdownPreviewCtrl.java
@@ -1,19 +1,49 @@
 package client.scenes;
 
+import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
+import com.vladsch.flexmark.ext.tables.TablesExtension;
+import com.vladsch.flexmark.ext.emoji.EmojiExtension;
+import com.vladsch.flexmark.ext.youtube.embedded.YouTubeLinkExtension;
+import com.vladsch.flexmark.ext.superscript.SuperscriptExtension;
+import com.vladsch.flexmark.ext.footnotes.FootnoteExtension;
+
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.ast.Node;
+import com.vladsch.flexmark.util.data.MutableDataSet;
+
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
-import one.jpro.platform.mdfx.MarkdownView;
+import javafx.scene.web.WebView;
+
+import java.util.Arrays;
 
 public class MarkdownPreviewCtrl {
     @FXML
     TextArea noteContents;
     @FXML
-    MarkdownView markdownView;
+    WebView markdownView;
 
     public void updateMarkdown() {
         Platform.runLater(()-> {
-            markdownView.setMdString(noteContents.getText());
+            MutableDataSet options = new MutableDataSet();
+
+            options.set(Parser.EXTENSIONS, Arrays.asList(
+                    TablesExtension.create(),
+                    StrikethroughExtension.create(),
+                    EmojiExtension.create(),
+                    YouTubeLinkExtension.create(),
+                    SuperscriptExtension.create(),
+                    FootnoteExtension.create()
+            ));
+            options.set(HtmlRenderer.SOFT_BREAK, "<br />\n");
+
+            Parser parser = Parser.builder(options).build();
+            HtmlRenderer renderer = HtmlRenderer.builder(options).build();
+            Node document = parser.parse(noteContents.getText());
+            String html = renderer.render(document);
+            markdownView.getEngine().loadContent(html);
         });
     }
 }
