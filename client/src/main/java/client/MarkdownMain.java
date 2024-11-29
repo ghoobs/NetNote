95ec5ebcdc48e5a0179ef7408a52def3da6fe480
Tristan Fransen <t.a.w.d.fransen@student.tudelft.nl>
2024-11-29 14:19:10 +0100

    Added markdown renderer with MDFX (MarkdownView)

--- client/src/main/java/client/MarkdownMain.java
+++ client/src/main/java/client/MarkdownMain.java
@@ -15,23 +15,21 @@
  */
 package client;
 
-import static com.google.inject.Guice.createInjector;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-
-import client.scenes.MarkdownPreviewCtrl;
-import com.google.inject.Injector;
-
 import client.scenes.AddQuoteCtrl;
-import client.scenes.MainCtrl;
+import client.scenes.MarkdownPreviewCtrl;
 import client.scenes.QuoteOverviewCtrl;
 import client.utils.ServerUtils;
+import com.google.inject.Injector;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static com.google.inject.Guice.createInjector;
+
+public class MarkdownMain extends Application {
 
 	private static final Injector INJECTOR = createInjector(new MyModule());
 	private static final MyFXML FXML = new MyFXML(INJECTOR);
@@ -42,18 +40,10 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		var mdPreview = FXML.load(MarkdownPreviewCtrl.class, "client", "scenes", "MarkdownPreview.fxml");
 
-		var serverUtils = INJECTOR.getInstance(ServerUtils.class);
-		if (!serverUtils.isServerAvailable()) {
-			var msg = "Server needs to be started before the client, but it does not seem to be available. Shutting down.";
-			System.err.println(msg);
-			return;
-		}
-
-		var overview = FXML.load(QuoteOverviewCtrl.class, "client", "scenes", "QuoteOverview.fxml");
-		var add = FXML.load(AddQuoteCtrl.class, "client", "scenes", "AddQuote.fxml");
-
-		var mainCtrl = INJECTOR.getInstance(MainCtrl.class);
-		mainCtrl.initialize(primaryStage, overview, add);
+		primaryStage.setTitle("Note Editor");
+		primaryStage.setScene(new Scene(mdPreview.getValue()));
+		primaryStage.show();
 	}
 }
