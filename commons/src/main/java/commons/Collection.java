d62b46b22e1c5bf33a0f860855bbc67ffc5b1c95
Kaja Pasek-Kozlowska <K.Pasek-Kozlowska@student.tudelft.nl>
2024-12-01 15:06:51 +0100

    Added the Collection entity

---
+++ commons/src/main/java/commons/Collection.java
@@ -0,0 +1,97 @@
+package commons;
+
+import jakarta.persistence.*;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Collection class.
+ * Data oriented for database usage.
+ */
+@Entity
+public class Collection {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long id;
+
+    public String name;
+
+    @OneToMany
+    public List<Note> notes;
+
+    /**
+     * Instantiates a new Collection
+     * Needed for Spring
+     */
+    public Collection() {
+    }
+
+    /**
+     * Instantiates a new empty Collection with a name
+     *
+     * @param name the name
+     */
+    public Collection(String name) {
+        this.name = name;
+        this.notes = new ArrayList<>();
+    }
+
+    /**
+     * Instantiates a new Collection with a name and Notes.
+     *
+     * @param name the name
+     * @param notes  the Notes
+     */
+    public Collection(String name, List<Note> notes) {
+        this.name = name;
+        this.notes = notes;
+    }
+
+    /**
+     * Compares two Collections
+     *
+     * @param obj other
+     * @return true if the contents are equal
+     */
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    /**
+     * Generates a hash code
+     *
+     * @return a unique integer based off the contents of this Collection
+     */
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    /**
+     * A preview of the contents of this Collection
+     *
+     * @return name of the Collection and the titles of the Notes it contains
+     */
+    @Override
+    public String toString() {
+        String result = "";
+        if (name.isEmpty()) {
+            result += "Unnamed Collection";
+        } else {
+            result += name;
+        }
+        result += "\n";
+        for(Note note : notes){
+            if (note.title.isEmpty()) {
+                result += "Unnamed Note\n";
+            } else {
+                result += note.title + "\n";
+            }
+        }
+        return result;
+    }
+}
---
+++ commons/src/test/java/commons/CollectionTest.java
@@ -0,0 +1,47 @@
+package commons;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.List;
+
+public class CollectionTest {
+
+    private Note note1;
+    private Note note2;
+    private Collection collection1;
+    private Collection collection2;
+
+    @BeforeEach
+    void setUp() {
+        note1 = new Note(
+                "Grocery List",
+                """
+                        - Milk
+                        - Bananas
+                        - Butter"""
+        );
+        note2 = new Note("", "");
+
+        collection1 = new Collection("Test1", List.of(note1, note2));
+        collection2 = new Collection("Test1", List.of(note1, note2));
+    }
+
+
+    @Test
+    void testToString() {
+        String expected = "Test1\nGrocery List\nUnnamed Note\n";
+        assertEquals(expected, collection1.toString());
+    }
+
+    @Test
+    void testEquals() {
+        assertEquals(collection1, collection2);
+    }
+
+    @Test
+    void testHashCode() {
+        assertEquals(collection1.hashCode(), collection2.hashCode());
+    }
+}
