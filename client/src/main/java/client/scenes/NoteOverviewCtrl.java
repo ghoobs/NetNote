6865baada4355ad649c49dc1841f8204f78dfa32
Kaja Pasek-Kozlowska <K.Pasek-Kozlowska@student.tudelft.nl>
2024-12-14 20:53:24 +0100

    More shortcuts and error messages for wrong titles

--- client/src/main/java/client/scenes/NoteOverviewCtrl.java
+++ client/src/main/java/client/scenes/NoteOverviewCtrl.java
@@ -9,7 +9,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.web.WebView;
@@ -18,6 +18,7 @@ import javafx.stage.Modality;
 import java.net.URL;
 //import java.util.Arrays;
 //import java.util.List;
+import java.util.List;
 import java.util.ResourceBundle;
 //import java.util.stream.Collectors;
 
@@ -40,8 +41,8 @@ public class NoteOverviewCtrl implements Initializable {
     private Button editButton;
     @FXML
     private Button addButton;
-//    @FXML
-//    private TextField searchBar;
+    @FXML
+    private TextField searchBar;
 //    @FXML
 //    private Button searchButton;
 //    @FXML
@@ -122,22 +123,57 @@ public class NoteOverviewCtrl implements Initializable {
     /**
      * Saves changes made to the selected note and updates the server when the "Save" button is clicked.
      * Ensures the note is set to non-editable mode after saving.
+     * The title of the note can't be empty and has to be unique.
      */
     public void savingNote() {
         Note noteSelected = listNotes.getSelectionModel().getSelectedItem();
         if (noteSelected != null) {
-            noteSelected.setText(noteWriting.getText());
-            noteSelected.setTitle(titleWriting.getText());
-            try {
-                server.updateNote(noteSelected);
+            if(!titleWriting.getText().isEmpty()) {
+                if(!allTitles().contains(titleWriting.getText())) {
+                    noteSelected.setText(noteWriting.getText());
+                    noteSelected.setTitle(titleWriting.getText());
+                    try {
+                        server.updateNote(noteSelected);
+                    } catch (Exception e) {
 
-            } catch (Exception e) {
-
+                    }
+                    listNotes.refresh();
+                    noteWriting.setEditable(false);
+                    titleWriting.setEditable(false);
+                }
+                else{
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Title Already Exists");
+                    alert.setHeaderText("The title of your note has to be unique!");
+                    alert.showAndWait().ifPresent(response -> {
+                        if (response == ButtonType.OK) {
+                            System.out.println("test");
+                        }
+                    });
+                }
             }
-            listNotes.refresh();
-            noteWriting.setEditable(false);
-            titleWriting.setEditable(false);
+            else{
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Empty Title");
+                alert.setHeaderText("The title of your note can't be empty!");
+                alert.showAndWait().ifPresent(response -> {
+                    if (response == ButtonType.OK) {
+                        System.out.println("test");
+                    }
+                });
+            }
         }
+    }
+
+    /**
+     * Returns a list of titles, only used internally in the savingNote function
+     *
+     * @return a List of titles
+     */
+    private List<String> allTitles(){
+        return listNotes.getItems().stream()
+                .map(n -> n.getTitle())
+                .toList();
     }
 
     /**
@@ -165,11 +201,12 @@ public class NoteOverviewCtrl implements Initializable {
      */
     public void onNoteClicked(MouseEvent mouseEvent) {
         Note noteSelected = listNotes.getSelectionModel().getSelectedItem();
-        noteWriting.setEditable(false);
-        titleWriting.setEditable(false);
-        noteWriting.setText(noteSelected.getText());
-        titleWriting.setText(noteSelected.getTitle());
-
+        if(noteSelected != null) {
+            noteWriting.setEditable(false);
+            titleWriting.setEditable(false);
+            noteWriting.setText(noteSelected.getText());
+            titleWriting.setText(noteSelected.getTitle());
+        }
     }
 
     /**
@@ -183,11 +220,25 @@ public class NoteOverviewCtrl implements Initializable {
 
     /**
      * Handles keyboard shortcuts for refreshing and
-     * saving, editing, adding and deleting (soon) notes
+     * saving, editing, adding and deleting (soon) notes.
+     * Pressing ESC sets the input focus to the search bar.
+     * Allows user to go through the list of notes using arrows and open them using O
      *
      * @param keyEvent the key event triggered by the user
      */
     public void keyPressed(KeyEvent keyEvent) {
+        if(keyEvent.getCode() == KeyCode.ESCAPE){
+            searchBar.requestFocus();
+        }
+        if(listNotes.isFocused() && keyEvent.getCode() == KeyCode.O){
+            Note noteSelected = listNotes.getSelectionModel().getSelectedItem();
+            if(noteSelected != null) {
+                noteWriting.setEditable(false);
+                titleWriting.setEditable(false);
+                noteWriting.setText(noteSelected.getText());
+                titleWriting.setText(noteSelected.getTitle());
+            }
+        }
         if(keyEvent.isShortcutDown()) {
             switch (keyEvent.getCode()) {
                 case S:
