f7d224416f4ebd59caef50079c8e052deaab774e
Chahid Boukachar <M.C.Boukachar@student.tudelft.nl>
2025-01-05 23:51:25 +0100

    I added most of the websocket, almost completely finishing the back-end of this feature

---
+++ server/src/main/java/server/websocket/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package server.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * This is the general configuration of the WebSocket used for Automated Change Synchronization
+ * This web socket will function as a way to push updates to the client
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+    // Websocket endpoint is, as seen below, /ws
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+    }
+}
