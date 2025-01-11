29b87abde0ebe25c9ecc69e101d9605f22957dae
Tristan Fransen <t.a.w.d.fransen@student.tudelft.nl>
2025-01-11 16:01:41 +0100

    Added keyword search tests

--- commons/src/test/java/commons/NoteTest.java
+++ commons/src/test/java/commons/NoteTest.java
@@ -99,4 +99,70 @@ public class NoteTest {
 
         assertEquals(note.hashCode(), note2.hashCode());
     }
+
+    @Test
+    void testKeywordEfficacyOnlyTags() {
+        Note note = new Note(
+                "Grocery List",
+                """
+                        - Milk
+                        - Bananas
+                        - Butter"""
+        );
+        note.tags.add(new Tag("Saturday"));
+        note.tags.add(new Tag("Food"));
+        note.tags.add(new Tag("Shopping"));
+        assertTrue(note.hasKeyword("Saturday"));
+        assertTrue(note.hasKeyword("FOod"));
+        assertTrue(note.hasKeyword("shOpping"));
+        assertFalse(note.hasKeyword("Chocolate"));
+    }
+
+    @Test
+    void testKeywordEfficacyOnlyTitle() {
+        Note note = new Note(
+                "Grocery List",
+                """
+                        - Milk
+                        - Bananas
+                        - Butter"""
+        );
+        assertTrue(note.hasKeyword("grocery"));
+        assertTrue(note.hasKeyword("LIST"));
+        assertFalse(note.hasKeyword("What"));
+    }
+
+    @Test
+    void testKeywordEfficacyOnlyBody() {
+        Note note = new Note(
+                "Grocery List",
+                """
+                        - Milk
+                        - Bananas
+                        - Butter"""
+        );
+        assertTrue(note.hasKeyword("Milk"));
+        assertTrue(note.hasKeyword("BANANAs"));
+        assertTrue(note.hasKeyword("utter"));
+        assertFalse(note.hasKeyword("?"));
+    }
+
+    @Test
+    void testKeywordEfficacyInvertedSearch() {
+        Note note = new Note(
+                "Grocery List",
+                """
+                        - Milk
+                        - Bananas
+                        - Butter"""
+        );
+        note.tags.add(new Tag("Saturday"));
+        note.tags.add(new Tag("Food"));
+        note.tags.add(new Tag("Shopping"));
+
+        assertTrue(note.hasKeyword("Grocery list Items"));
+        assertTrue(note.hasKeyword("Saturday morning food shopping"));
+        assertTrue(note.hasKeyword("chocolate food"));
+        assertFalse(note.hasKeyword("chocolate cake"));
+    }
 }
